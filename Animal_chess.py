c5469fde2a3b1ad5f690f6e7a900dfb8619bb569
VironLee <hhelipeb@protonmail.com>
2021-11-24 20:23:19 +0800

    :fire:

--- Animal_chess.py
+++ Animal_chess.py
@@ -1,4 +1,5 @@
 import copy
+from enum import Enum
 import random
 
 from Base import Grid, GridProperty, Map, position
@@ -25,7 +26,6 @@ def generate_random_animals(gameMap: Map) -> dict:
     # 初始动物位置只能在land上
     animal_dict = {}
 
-    # todo:须新增其他动物的自动生成逻辑
     a = random.randint(1, len(temp_lands) - 2)  # 随机生成老虎数量
     for i in range(0, a):  # 确定老虎在字典中的位置
         pos = temp_lands[i]
@@ -134,83 +134,110 @@ class Animal_chess:
 
         print(situation)
 
-    def VitalCheck(self, id):
-        obj: Animal
+    def check_range(self, map: Map, des: position) -> bool:
+        des_x = des.x
+        des_y = des.y
+        row = map.rows
+        column = map.columns
+
+        if des_x >= column or des_y >= row:
+            return False
+
+        elif des_x < 0 or des_y < 0:
+            return False
+
+        else:
+            return True
+        
+    def rat_check_valid(self, org: position, delta_x: int, delta_y: int) -> bool:
         rival: Animal
-        des: Grid
-        org: Grid
+
+        # 计算终点坐标
+        des_x = org.x + delta_x
+        des_y = org.y + delta_y
+
+        # 通过终点坐标找到地块
+        des = Animal_chess.gameMap.gridmatrix[des_y][des_x]
+
+        # 查询地块属性
+        if des.property == GridProperty.target:
+            return True
+
+        elif des.property == GridProperty.land:
+            if des.OwnerId:
+                rival = self.animal_dict.get(des.OwnerId)
+            
+                if rival.type == "tiger":
+                    return False
+                
+                if rival.type == "elephant":
+                    return True
+
+                if rival.type == "rat":
+                    return True
+
+            else:
+                return True
+
+        else:
+            return False
+        
+    def rat_fight(self: Animal) -> Enum:
+        rival: Animal
+        win = 1
+        lose = 0
+        des = Animal_chess.gameMap.gridmatrix[7][7]
+        rival = Animal_chess.animal_dict.get(des.OwnerId)
+        
+        if rival.type == "elephant":
+            return win
+            
+        if rival.type == "rat":
+            if self.Combat_Effectiveness > rival.Combat_Effectiveness:
+                return win
+
+            if self.Combat_Effectiveness < rival.Combat_Effectiveness:
+                return lose
+                        
+            # 战力相同一起卒，更新地块信息
+            # TODO：这里可以引入随机数去决定战斗结果
+            if self.Combat_Effectiveness == rival.Combat_Effectiveness:
+                return lose
+        
+    def rat_move(self):
+        result = self.rat_check_valid(position(2, 4), 5, 3) and self.check_range(Map, position(7, 7))
+        
+        if result:
+            Rat.move()
+
+        else:
+            Rat.stop()
+
+    def find_an_object(self, id):
+        obj: Animal
         self.id = id
 
         # 输入一个id，生成其对应的动物对象
-        obj = self.animal_dict.get(self.id)
+        obj = Animal_chess.animal_dict.get(self.id)
         
-        # 生成动物对象所在地的地块对象
-        org_x = obj.pos.gotX()
-        org_y = obj.pos.gotY()
-        org = Map.gridmatrix[org_x][org_y]
-
-        # 生成目的地的地块对象
-        obj.pos.move()
-        des_x = obj.pos.gotX()
-        des_y = obj.pos.gotY()
-        des = Map.gridmatrix[des_x][des_y]
-
-        # 老鼠类的判定方法
         if obj.type == "rat":
+            result = self.rat_move()
 
-            # 目标点属性不为0，invalid的情况
-            if des.property:
-                if des.property == GridProperty.trap:
-                    obj.getTrapped()
+            if result:
+                fight_result = self.rat_fight()
 
-                    ### 下边这行用哪种表达方式可行？Answer:按照No.150行的语句来看，这两种写法是等价的
-                    Map.gridmatrix[des_x][des_y].OwnerId = None
-                    # org.OwnerId = None
+                if fight_result:
+                    Animal_chess.gameMap.put_animal(position(7, 7), self.id)
 
-                # 掉河里的状态没想好，暂定返回空值
-                if des.property == GridProperty.river:
-                    return None
-            
-            # 目标点属性为0，valid的情况，先清除原有信息
-            else:
-                org.OwnerId = None
+                else:
+                    obj.wasHunted()
 
-                # 恰好移动到目标点，结束游戏
-                #TODO：Target的value好像是3，不是0，是进不了这个else分支的
-                if des.property == GridProperty.target: #TODO:即使到了Target也要先更新信息，再宣告游戏结束;完成Endthegame()的实现
-                    Animal_chess.Endthegame()
-                    
-                # 移动到空地，更新地块占领信息
-                if des.OwnerId == None:
-                    des.isOccupiedBy(obj.id)
 
-                # 不为空地，通过占有者信息判断状态
-                if des.OwnerId:
+        if obj.type == "tiger":
+            self.tiger_move()
 
-                    # 生成占有地块的动物对象
-                    rival = self.animal_dict.get(des.OwnerId)
+        if obj.type == "elephant":
+            self.elephant_move()
 
-                    if rival.type == "tiger":
-                        obj.wasHunted()
-                        
-                    if rival.type == "elephant":
-                        rival.wasHunted()
-                        des.isOccupiedBy(obj.id)
-                    
-                    # 同类打架看战力，战力小的淘汰
-                    if rival.type == "rat":
-                        if obj.Combat_Effectiveness > rival.Combat_Effectiveness:
-                            rival.wasHunted()
-                            des.isOccupiedBy(obj.id)
-
-                        if obj.Combat_Effectiveness < rival.Combat_Effectiveness:
-                            obj.wasHunted()
-                        
-                        # 战力相同一起卒，更新地块信息
-                        # TODO：这里可以引入随机数去决定战斗结果
-                        if obj.Combat_Effectiveness == rival.Combat_Effectiveness:
-                            obj.wasHunted()
-                            rival.wasHunted()
-                            des.OwnerId = None
 
         #TODO：其他动物类的判定方法(封装）
